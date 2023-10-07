4f4a919cda4ea8d13b65ba338a4e5a9bc2e92419
Serhat Koçhan <serhatkochan@hotmail.com.tr>
2023-10-07 19:51:33 +0300

    init

---
+++ src/test/java/tr/scrumplanner/venus/VenusApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.scrumplanner.venus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
